652a1c2791d79eb19c8741f73db77f7843045994
Anthony Sims Jr <anthony.sims.226@gmail.com>
2017-06-20 14:57:52 -0400

    grab 100 tweets and push text into new array

--- app.js
+++ app.js
@@ -13,9 +13,14 @@ var client = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-// GRAB LAST MONTH'S TWEETS FROM USER TIMELINE AND DO STUFF TO THEM
-client.get('statuses/user_timeline', {screen_name: 'twitterdev', count: 1}, function(error, tweets, response) {
+// GRAB LAST 100 TWEETS FROM USER TIMELINE AND DO STUFF TO THEM
+client.get('statuses/user_timeline', {screen_name: 'twitterdev', count: 100, trim_user: true}, function(error, tweets, response) {
   if(error) throw error;
-  console.log(tweets[0].text);
+  var tweetsArray = [];
+  for(var i = 0; i < tweets.length; i++) {
+    tweetsArray.push(tweets[i].text);
+    console.log((i + 1) + ": " + tweetsArray[i] + "\n");
+    // console.log((i + 1)+ ": " + tweets[i].text);
+  }
 
 });
