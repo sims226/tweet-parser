25fcb18fa7561478e6af5a8697b8808e95c72cc2
Anthony Sims Jr <anthony.sims.226@gmail.com>
2017-06-20 19:33:12 -0400

    add tweet parser and punctuation counter

--- app.js
+++ app.js
@@ -14,13 +14,70 @@ var client = new Twitter({
 });
 
 // GRAB LAST 100 TWEETS FROM USER TIMELINE AND DO STUFF TO THEM
-client.get('statuses/user_timeline', {screen_name: 'twitterdev', count: 100, trim_user: true}, function(error, tweets, response) {
+client.get('statuses/user_timeline', {screen_name: 'sims226', count: 100, trim_user: true}, function(error, tweets, response) {
   if(error) throw error;
+
+  // LOOP OVER RESPONSE OBJECT AND PUSH TWEET TEXT INTO NEW ARRAY
   var tweetsArray = [];
   for(var i = 0; i < tweets.length; i++) {
     tweetsArray.push(tweets[i].text);
-    console.log((i + 1) + ": " + tweetsArray[i] + "\n");
-    // console.log((i + 1)+ ": " + tweets[i].text);
+    // console.log((i + 1) + ": " + tweetsArray[i] + "\n");
   }
+
+  // COUNT ALL THE PUNCTUATION IN THE TWEETS AND COUNT THE RESULTS
+  var commas        = 0;
+  var periods       = 0;
+  var questions     = 0;
+  var exclamations  = 0;
+  var dashes        = 0;
+  var colons        = 0;
+  var semicolons    = 0;
+  var apostrophes   = 0;
+
+  tweetsArray.map( function(text) {
+    for (var i = 0; i < text.length; i++) {
+      switch (text[i]) {
+        case "."  :
+          periods++;
+          break;
+        case ","  :
+          commas++;
+          break;
+        case "?"  :
+          questions++;
+          break;
+        case "!"  :
+          exclamations++;
+          break;
+        case "-"  :
+          dashes++;
+          break;
+        case ":"  :
+          colons++;
+          break;
+        case ";"  :
+          semicolons++;
+          break;
+        case "'"  :
+          apostrophes++;
+          break;
+      }
+    }
+  });
+  console.log("\n");
+  console.log("_______________________\n");
+  console.log("YOUR PUNCTUATION COUNTS\r");
+  console.log("_______________________\r");
+  console.log("\n");
+  console.log("PERIODS: " + periods);
+  console.log("COMMAS: " + commas);
+  console.log("QUESTIONS: " + questions);
+  console.log("EXCLAMATIONS: " + exclamations);
+  console.log("DASHES: " + dashes);
+  console.log("COLONS: " + colons);
+  console.log("SEMICOLONS: " + semicolons);
+  console.log("APOSTROPHES: " + apostrophes);
+
+
 
 });
