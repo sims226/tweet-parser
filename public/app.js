f83bc2dfa3f646be5328cf8debb37cffaddf99d7
Anthony Sims Jr <anthony.sims.226@gmail.com>
2017-07-01 15:39:22 -0400

    Send backend data to front end in the most spartan way thinkable.

---
+++ public/app.js
@@ -0,0 +1,14 @@
+document.ready(function() {
+  // GRAB USER INPUT AND STORE IT
+  var screenName = "";
+  function grabInput() {
+    screenName = getElementById("userInput").value;
+    console.log(screenName + "stored in variable.");
+  }
+
+  // SEND REQUEST TO BACK END
+  
+
+
+
+});
