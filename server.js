6782ed827557a0a4cdd3509974b6fdcca96d7bc7
Anthony Sims Jr <anthony.sims.226@gmail.com>
2017-07-05 19:03:09 -0400

    minor commit before branching

--- server.js
+++ server.js
@@ -1,16 +1,56 @@
 // MODULE DEPENDENCIES
 var dotenv = require('dotenv');
 var Twitter = require('twitter');
-var express = require('express');
-var app = express();
+var fs = require("fs");
+var path = require("path");
+var mime = require("mime");
 
 // LOAD ENV VARIABLES
 dotenv.load();
 
-// START A SERVER
-app.listen(3000, function () {
-  console.log('Listening on port 3000');
+// BASIC PAGE SERVING FUNCTIONALITY
+function send404(response) {
+  response.writeHead(404, {"Content-type" : "text/plain"});
+  response.write("Error 404: resource not found");
+  response.end();
+}
+
+function sendPage(response, filePath, fileContents) {
+  response.writeHead(200, {"Content-type" : mime.lookup(path.basename(filePath))});
+  response.end(fileContents);
+}
+
+function serverWorking(response, absPath) {
+  fs.exists(absPath, function(exists) {
+    if (exists) {
+      fs.readFile(absPath, function(err, data) {
+        if (err) {
+          send404(response);
+        } else {
+          sendPage(response, absPath, data);
+        }
+      });
+    } else {
+      send404(response);
+    }
+  });
+}
+
+// HTTP SERVER AND BASIC ROUTES
+var server = http.createServer(function(request, response) {
+  var filePath = false;
+
+  if (request.url == '/') {
+    filePath = "public/index.html";
+  } else {
+    filePath = "public" + request.url;
+  }
+
+  var absPath = "./" + filePath;
+  serverWorking(response, absPath);
 });
+
+var port_number = server.listen(process.env.PORT || 3000);
 
 // MAKE A TWITTER CLIENT
 var client = new Twitter({
